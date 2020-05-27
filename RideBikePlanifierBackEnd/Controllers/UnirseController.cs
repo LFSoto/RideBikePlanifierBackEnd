334fbb06b358803869a9f4ef1a59f49454e34f5d
LFSoto <soto2197@gmail.com>
2020-05-26 20:45:29 -0600

    Actualizacion acumulativa

--- RideBikePlanifierBackEnd/Controllers/UnirseController.cs
+++ RideBikePlanifierBackEnd/Controllers/UnirseController.cs
@@ -46,7 +46,7 @@ namespace RideBikePlanifierBackEnd.Controllers
             return finales;
         }
 
-        [HttpGet("/UnirseUsuario/{id}")]
+        [HttpGet("/api/UnirseUsuario/{id}")]
         public async Task<ActionResult<List<int>>> GetUnirseUsuario(string id)
         {
             List<UsuarioRuta> usuarioRutas = await _context.usuarioRutas
